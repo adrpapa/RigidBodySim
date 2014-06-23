857bb59223ccd64e11e0999254482729dbf0b028
Adriano <adrpapa@gmail.com>
2014-06-23 13:23:57 -0300

    first commit

---
+++ RigidBodySim/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  RigidBodySim
+//
+//  Created by Adriano Papa on 6/23/14.
+//  Copyright (c) 2014 Adriano Papa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
